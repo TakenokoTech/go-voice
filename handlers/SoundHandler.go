b908727ad76c364fa606f33ca7081f86561405ce
takenoko <takenoko.tech@gmail.com>
2019-07-07 13:54:48 +0900

    add anlytics

--- handlers/SoundHandler.go
+++ handlers/SoundHandler.go
@@ -41,8 +41,15 @@ func SoundHandler(w http.ResponseWriter, r *http.Request) {
 		f32 := request.Sound[i : i+chunk]
 		f64 := utils.Float32To64(f32)
 		c128 := utils.Float64ToComplex128(f64)
+		// フーリエ
 		ff := fft.FFT(c128)
-		ff = effect(ff)
+		// アナライズ
+		anlytics(ff)
+		for index, frame := range ff {
+			ff[index] = effect(index, frame)
+		}
+		anlytics(ff)
+		// 逆フーリエ
 		iff := fft.IFFT(ff)
 		buffer = append(buffer, utils.Complex128ToFloat32(iff)...)
 	}
@@ -53,6 +60,46 @@ func SoundHandler(w http.ResponseWriter, r *http.Request) {
 	utils.ResponseSuccess(w, res)
 }
 
-func effect(sound []complex128) []complex128 {
-	return sound
+func effect(f int, frame complex128) complex128 {
+	if f < 16 || f > 1024-16 {
+		return complex128(0)
+	}
+	return frame
+}
+
+func anlytics(sound []complex128) {
+	maxReI, minReI := 0, 0
+	maxImI, minImI := 0, 0
+	maxRe, minRe := float64(-100000), float64(100000)
+	maxIm, minIm := float64(-100000), float64(100000)
+	for i, frame := range sound {
+		re := real(sound[i])
+		im := imag(sound[i])
+		if maxRe < re {
+			maxRe = re
+			maxReI = i
+		}
+		if minRe > re {
+			minRe = re
+			minReI = i
+		}
+		if maxIm < im {
+			maxIm = im
+			maxImI = i
+		}
+		if minIm > im {
+			minIm = im
+			minImI = i
+		}
+
+		if real(frame) > 10 {
+			log.Printf("[Frame] [%4d]%3f", i, frame)
+		}
+	}
+
+	if maxRe > 5 {
+		log.Printf("[Real] max: [%4d]%3f, min: [%4d]%3f", maxReI, maxRe, minReI, minRe)
+		log.Printf("[Imag] max: [%4d]%3f, min: [%4d]%3f", maxImI, maxIm, minImI, minIm)
+		log.Printf("====================================")
+	}
 }
